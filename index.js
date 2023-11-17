4ef284b50101921df785bf4448c575dc3979b211
Janzen Aguila <janzenaguila@gmail.com>
2023-11-17 20:33:54 +0800

    updated the pdf text extractor function

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const pdfUtil = require("pdf-to-text");
+const crawler = require("crawler-request");
 const app = express();
+const options = {from: 0, to: 10};
+
+app.get("/extractPDFText", async (req, res) => {
+    const pdfResult = await crawler(req.query.url);
+    res.send({text: pdfResult.text});
+});
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello world</h1>");
