10ee916f081c147e1bade726add9a0b2cb2d515a
Janzen Aguila <janzenaguila@gmail.com>
2023-11-28 20:39:24 +0800

    Added functions for CSV
    1. Added a data.csv for writing
    2. Added an endpoint to read CSV
    3. Added an endpoint to reset the CSV file
    4. Added an endpoint to append to the CSV file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const fs = require("fs");
 const crawler = require("crawler-request");
 const bodyParser = require("body-parser");
 const app = express();
@@ -12,6 +13,32 @@ app.use(bodyParser.json());
 app.post("/extractPDFText", async (req, res) => {
     const pdfResult = await crawler(req.body.url);
     res.send({text: pdfResult.text});
+});
+
+app.post("/addToCSV", async (req, res) => {
+    let { user, rating, details, timestamp } = req.body;
+    const data = `"${user}","${rating}","${details}","${timestamp}"\n`;
+
+    try {
+        fs.appendFileSync("data.csv", data);
+        res.send("Data saved successfully.");
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+app.post("/readFromCSV", async (req, res) => {
+    fs.readFile("data.csv", "utf-8", (err, data) => {
+        if (err) console.log(err);
+        else res.send(data);
+    });
+});
+
+app.post("/resetCSV", async (req, res) => {
+    fs.writeFile("data.csv", "user,rating,details,timestamp\n", "utf-8", (err) => {
+        if (err) console.log(err);
+        else res.send("Data saved");
+    });
 });
 
 app.get("/", (req, res) => {
